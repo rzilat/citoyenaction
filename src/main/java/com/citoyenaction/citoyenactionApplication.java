508577e17530564a3386ef4802cd3e30d9768c31
redao <redao@DESKTOP-LOCABFL>
2020-04-11 16:25:27 +0100

    initial first commit

---
+++ src/main/java/com/citoyenaction/citoyenactionApplication.java
@@ -0,0 +1,16 @@
+package com.citoyenaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class citoyenactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(citoyenactionApplication.class, args);
+
+	}
+
+}
