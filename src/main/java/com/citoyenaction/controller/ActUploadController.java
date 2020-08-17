496062bb4ae7b0c690730acaa4cb70f2cb36594c
rzilat <36553908+rzilat@users.noreply.github.com>
2020-08-18 00:17:10 +0100

    Update ActUploadController.java

--- src/main/java/com/citoyenaction/controller/ActUploadController.java
+++ src/main/java/com/citoyenaction/controller/ActUploadController.java
@@ -29,7 +29,7 @@ public class ActUploadController {
 	private ActUploadService actUploadService;
 	
 	@RequestMapping(value= "/actupload", method= RequestMethod.POST,headers = "content-type=multipart/*")
-	public ResponseEntity <ActUpload> saveActUpload(HttpServletRequest request, @RequestPart(required = true) MultipartFile file ) throws IOException {
+	public ResponseEntity <ActUpload> saveActUpload(@RequestPart String description, @RequestPart(required = true) MultipartFile file ) throws IOException {
 		String fileName = file.getOriginalFilename();
 		byte[] fileData = file.getBytes();
 		ActUpload actUpload = new ActUpload();
