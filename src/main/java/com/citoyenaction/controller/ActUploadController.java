f684add2b5db7d47c44b82224fb3d087e1a13a5b
rzilat <36553908+rzilat@users.noreply.github.com>
2020-08-24 23:31:54 +0100

    Update ActUploadController.java
    remove requerd

--- src/main/java/com/citoyenaction/controller/ActUploadController.java
+++ src/main/java/com/citoyenaction/controller/ActUploadController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -30,7 +29,7 @@ public class ActUploadController {
 	private ActUploadService actUploadService;
 	
 	@RequestMapping(value= "/actupload", method= RequestMethod.POST,headers = "content-type=multipart/*")
-	public ResponseEntity <String> saveActUpload(@RequestParam String description, @RequestParam(required = true) MultipartFile file ) throws IOException {
+	public ResponseEntity <ActUpload> saveActUpload(@RequestParam String description, @RequestPart MultipartFile file ) throws IOException {
 		String fileName = file.getOriginalFilename();
 		byte[] fileData = file.getBytes();
 		ActUpload actUpload = new ActUpload();
@@ -38,7 +37,8 @@ public class ActUploadController {
 		actUpload.setFileData(fileData);
 		actUpload = actUploadService.saveActUpload(actUpload);
 		//actUpload.toString();
-		return new ResponseEntity<> ("file uploaded",HttpStatus.OK);
+		return new ResponseEntity<> (actUpload,HttpStatus.OK);
+		
 	}
 	
 	
